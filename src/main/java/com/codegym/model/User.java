fe89c33bad2cacab01f36e34ccd79e86ddfb404a
AlexWoo176 <phuong.tuan2012@gmail.com>
2020-06-07 22:34:39 +0700

    [Bài tập] Validate form đăng ký

---
+++ src/main/java/com/codegym/model/User.java
@@ -0,0 +1,94 @@
+package com.codegym.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.*;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotEmpty
+    @Size(min = 2, max = 50)
+    private String firstName;
+    @NotEmpty
+    @Size(min = 2, max = 50)
+    private String lastName;
+
+    @NotEmpty
+    @Size(min = 10, max = 10)
+    @Pattern(regexp = "^[0]{1}[0-9]{9}$")
+    private String phoneNumber;
+
+    @Min(18)
+    @Max(150)
+    private int age;
+
+    @NotEmpty
+    @Email
+    private String email;
+
+    public User() {
+    }
+
+    public User(@NotEmpty @Size(min = 2, max = 50) String firstName, @NotEmpty @Size(min = 2, max = 50) String lastName, @NotEmpty @Size(min = 10, max = 10) @Pattern(regexp = "^[0]{1}[0-9]{9}$") String phoneNumber, @Min(18) @Max(150) int age, @NotEmpty @Email String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.age = age;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/main/java/com/codegym/services/impl/UserServiceImpl.java
@@ -0,0 +1,35 @@
+package com.codegym.services.impl;
+
+import com.codegym.model.User;
+import com.codegym.repositories.UserRepository;
+import com.codegym.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+public class UserServiceImpl implements UserService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public Page<User> findAll(Pageable pageable) {
+        return userRepository.findAll(pageable);
+    }
+
+    @Override
+    public User findById(Long id) {
+        return userRepository.findOne(id);
+    }
+
+    @Override
+    public void save(User user) {
+        userRepository.save(user);
+    }
+
+    @Override
+    public void remove(User user) {
+        userRepository.delete(user);
+    }
+}
